41621a790d6e408ee32ce841cad5fb38647ecfc4
igor <ibotov@os33.com>
2018-01-16 17:58:27 +0300

    T

--- Package.swift
+++ Package.swift
@@ -4,9 +4,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Clibproc",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    name: "Clibproc"
 )
